66ac8e4ad2310e574b17ce34eacd117eeb2f4da2
Dolapo Adebanjo <dolapomola@gmail.com>
2025-06-26 13:52:04 +0100

    modified how document is used in Modal component

--- src/components/global/Modal.tsx
+++ src/components/global/Modal.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
 interface ModalType {
@@ -8,6 +8,14 @@ interface ModalType {
 }
 
 const Modal: React.FC<ModalType> = ({ children, styling }) => {
+  const [mounted, setMounted] = useState(false);
+  
+    useEffect(() => {
+    // This will only run in the browser
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
   return createPortal(
     <div
       className={`fixed inset-0 z-[9999] flex items-center justify-center overflow-auto bg-black bg-opacity-50 ${styling}`}
